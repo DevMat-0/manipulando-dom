3679e9a08e7bd2acaa1ce8b9aab14d7612760081
Matheus <matheus.dte11@gmail.com>
2026-08-12 21:05:09 -0300

    Finalizado

--- js/fundo-aleatorio.js
+++ js/fundo-aleatorio.js
@@ -19,16 +19,13 @@ const anoCompleto = date.getFullYear()
 if (hora >= 6 && hora < 12){
 document.body.style.background = "linear-gradient(135deg, #FFAA65, #FFD180, #90CAF9)"
 
-document.body.style.backgroundImage = "url(../assets/imagem-manha.jpg)"
 
 } else if (hora >= 12 && hora < 18){
 document.body.style.background = "linear-gradient(145deg, #FF8A65, #FFB74D, #64B5F6)"
 
-document.body.style.backgroundImage = "url(../assets/imagem-tarde.jpg)"
 } else if (hora >= 18 && hora < 24){
 document.body.style.background = "linear-gradient(155deg, #000428, #211132, #004e92)"
 
-document.body.style.backgroundImage = "url(../assets/imagem-noitee.jpg)"
 } else {
     document.body.style.background = "linear-gradient(155deg, #0b7c51, #3f0d74, #183f60)"
 }
